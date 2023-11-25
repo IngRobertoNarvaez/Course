745a3cead9a8ca97a6940eecd072dd8b8ea9b9f6
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-11-25 14:28:36 -0500

    Directivas, cabecera

---
+++ directivas.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*Se crea una constrante con el PI con numero flotante*/
+#define PI 3.1416
+
+int main()
+{
+    /* %f servira para mostrar el valor*/
+    printf("El calor de nuestra constante Pi es %f", PI);
+    return 0;
+}
