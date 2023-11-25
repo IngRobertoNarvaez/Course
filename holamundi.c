b6306894df9bcdd2b57751452ff721a8892e357b
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-11-25 14:18:17 -0500

    Estructura base, holamundo

---
+++ holamundi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+Esto servira para dar un comentario.
+Estructura base para todo codigo.
+Seguimos un ejemplo del curso para lograr todo.
+*/
+/*
+/n Esto servira para dar un salto de linea.
+*/
+int main()
+{
+    printf("Hola Mundo\n");
+    printf("Mi nombre es Roberto");
+    return 0;
+}
